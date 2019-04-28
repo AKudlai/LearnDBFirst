321a25a9b69acb652f9bded7c9b287037f487a14
Alexander Kudlai <student@kudlai.com>
2019-04-28 15:04:09 +0300

    • Added table BookDetails. • Updated edmx from the database.

--- LearnDBFirst.DAL/DataEfContext.Context.cs
+++ LearnDBFirst.DAL/DataEfContext.Context.cs
@@ -26,5 +26,6 @@ namespace LearnDBFirst.DAL
         }
     
         public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<BookDetail> BookDetails { get; set; }
     }
 }
---
+++ LearnDBFirst.Database/Tables/BookDetails.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[BookDetails]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+    [Description] NVARCHAR(MAX),
+    [NumberOfPages] INT
+)
