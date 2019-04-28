3d162ca6b972a110c1939d5416d82f23bd87ee15
Alexander Kudlai <student@kudlai.com>
2019-04-28 23:37:42 +0300

    • Added vBook view with a book, book detail, authors and publisher information.

---
+++ LearnDBFirst.Database/Views/vBook.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[vBook]
+AS 
+SELECT b.[Name] AS [BookName], d.[Description], d.[NumberOfPages], a.[FirstName] + ', ' + a.[LastName] AS [AuthorsName], p.[Name] AS [PublisherName]
+FROM [Books] AS b, [BookDetails] AS d, [Authors] AS a, [Publishers] AS p;
