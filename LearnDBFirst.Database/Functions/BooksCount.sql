3f31d1afe36424838127296e8e727e98c6456d62
Alexander Kudlai <student@kudlai.com>
2019-04-29 18:16:12 +0300

    • Added DB function GetBooksCount returned books count relative author. • Updated edmx.

---
+++ LearnDBFirst.Database/Functions/BooksCount.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[BooksCount]
+(
+    @firstName NVARCHAR(256),
+    @lastName NVARCHAR(256)
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @result INT;
+    SET @result =
+    (SELECT COUNT([Name])
+    FROM [Books] AS b  
+    LEFT JOIN [Authors] AS a ON b.[Id] = a.[Id]
+    WHERE a.[FirstName] = @firstName  AND a.[LastName] = @lastName)
+    RETURN @result
+END
